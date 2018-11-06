b0d21aa6a67b31c5291a9c42ceb6d90aa50a48c0
ll9 <llwierer@web.de>
2018-11-06 20:15:26 +0100

    unclosable modal

---
+++ app/components/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <Page @loaded="loadPage">
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+import Page2 from './Page2';
+
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    },
+    methods: {
+        loadPage() {
+            this.$showModal(Page2, {cancelable: false})
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/components/Page2.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page @loaded="loadPage">
+        <StackLayout>
+            <Label class="message" :text="msg" col="0" row="0"/>
+            <Button @tap="$modal.close" text="Close"></Button>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    },
+    methods: {
+        loadPage(event) {
+            event.object._dialogFragment.getDialog().setCanceledOnTouchOutside(false);
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
